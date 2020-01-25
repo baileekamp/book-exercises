92475389c25a81b27fa0cda775ea832175868f7b
Bailee Kamp <baileebee9@yahoo.com>
2020-01-24 17:39:28 -0800

    Add chapter 8 exercise 1, chapter 10 exercise 2 bartledoo

--- chapter-08-exercises/exercise-1/exercise.R
+++ chapter-08-exercises/exercise-1/exercise.R
@@ -11,31 +11,46 @@ meals <- list(breakfast_lunch = my_breakfast, my_lunch)
 
 # Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
-meals <- list(sixteen_breakfast = my_breakfast, sixteen_lunch = my_lunch,sixteen_dinner = my_dinner <- list(dinner_food = "salmon", side_1 = "mashed potatoes", side_2 = "asparagus"))
+meals <- list(sixteen_breakfast = my_breakfast, sixteen_lunch = my_lunch, sixteen_dinner = my_dinner <- list(dinner_food = "salmon", side_1 = "mashed potatoes", side_2 = "asparagus"))
 
 # Use dollar notation to extract your `dinner` element from your list
 # and save it in a vector called 'dinner'
-dinner <- meals[["sixteen_dinner"]]
+dinner <- meals$sixteen_dinner
+print(dinner)
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-lunch <- meals[["sixteen_lunch"]]
+meals[[5]] <- meals[["sixteen_lunch"]]
+print(meals[[5]])
 
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
-early_meals <- meals["sixteen_breakfast", "sixteen_lunch"]
-
+early_meals <- meals[c("sixteen_breakfast", "sixteen_lunch")]
+print(early_meals)
 
 ### Challenge ###
 
 # Create a list that has the number of items you ate for each meal
 # Hint: use the `lappy()` function to apply the `length()` function to each item
+length(meals$sixteen_breakfast)
+length(meals$sixteen_lunch)
+length(meals$sixteen_dinner)
 
+num_items <- lapply(meals, length)
+print(num_items)
 
 # Write a function `add_pizza` that adds pizza to a given meal vector, and
 # returns the pizza-fied vector
+add_pizza <- function(meal_vector) {
+  # add pizza to that vector
+  new_vector <- c(meal_vector, "Pizza")
+  
+  # return the new vector
+  return(new_vector)
+}
 
+add_pizza(c("nachos"))
 
 # Create a vector `better_meals` that is all your meals, but with pizza!
-
+lapply(meals, add_pizza)
